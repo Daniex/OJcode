c147fe6cdaca111669dc733c89582282c8f18a06
Daniex <jjhdust@gmail.com>
2015-04-24 09:33:31 +0800

    Update Solution1.java

--- leetcode/algorithms/1.TwoSum/Solution1.java
+++ leetcode/algorithms/1.TwoSum/Solution1.java
@@ -1,28 +1,19 @@
 ﻿public class Solution1 {
     public int[] twoSum(int[] numbers, int target) {
-        int[] rst = new int[2];
-
-        int len = numbers.length;
-        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-
-        for (int i = 0; i < len; i++){
-            hm.put(numbers[i], i + 1);
-        }
-
-        for (int i = 0; i < len; i++){;
-            Integer j = hm.get(target - numbers[i]);
-            int position = i +1;
-            if (j != null && position != j) {
-                if (position < j) {
-                    rst[0] = position;
-                    rst[1] = j;
-                } else {
-                    rst[1] = position;
-                    rst[0] = j;
-                }
-                return rst;
-            }
-        }
-        return rst;
+	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	int[] result = new int[2];
+ 
+	for (int i = 0; i < numbers.length; i++) {
+		if (map.containsKey(numbers[i])) {
+			int index = map.get(numbers[i]);
+			result[0] = index+1 ;
+			result[1] = i+1;
+			break;
+		} else {
+			map.put(target - numbers[i], i);
+		}
+	}
+ 
+	return result;
     }
 }
