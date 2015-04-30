b8663423f2535b15afa5e80aaf4883d2de327c6c
Daniex <jjhdust@gmail.com>
2015-04-30 09:40:53 +0800

    add new java solution to 2. add two number

--- leetcode/algorithms/2.AddTwoNumbers/Solution1.java
+++ leetcode/algorithms/2.AddTwoNumbers/Solution1.java
@@ -46,3 +46,26 @@ public class AddTwoNumbers {
 		return head.next;
 	}
 }
+
+public class AddTwoNumbers {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode(-1);
+    ListNode current = dummy;
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int v1 = (l1 == null) ? 0 : l1.val, v2 = (l2 == null) ? 0 : l2.val;
+      int sum = v1 + v2 + carry;
+
+      carry = sum / 10;
+      current.next = new ListNode(sum % 10);
+
+      l1 = (l1 == null) ? null : l1.next;
+      l2 = (l2 == null) ? null : l2.next;
+      current = current.next;
+    }
+    if (carry > 0) {
+      current.next = new ListNode(carry);
+    }
+    return dummy.next;
+  }
+}
