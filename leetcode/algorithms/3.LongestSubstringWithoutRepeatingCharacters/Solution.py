f582bac94bd8ec737c1d58b9fdce8234adfb6f88
Daniex <jjhdust@gmail.com>
2015-05-10 17:20:36 +0800

    Create Solution.py

---
+++ leetcode/algorithms/3.LongestSubstringWithoutRepeatingCharacters/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def lengthOfLongestSubstring(self, s):
+        res = 0
+        cur = 0
+        d = {}
+        for i, c in enumerate(s):
+            if c not in d:
+                cur += 1
+            else:
+                cur = min(i - d[c], cur + 1)
+            d[c] = i
+            res = max(res, cur)
+        return res
